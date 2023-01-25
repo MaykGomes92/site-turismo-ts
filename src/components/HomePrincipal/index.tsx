fb682b2bac1aa6b40c55656f3ebb00b443c88170
Mayk Gomes <maykcontato98@hotmail.com>
2023-01-25 17:18:15 -0300

    .

---
+++ src/components/HomePrincipal/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.scss'
+const index = () => {
+  
+  return (
+    <main>
+      <header>
+        <img src='logo-area-1.png' alt='logo' />
+      </header>
+      <div className='descricao'>
+        <h1>Descubra o mundo com nossos pacotes de viagem personalizados!</h1>
+        <p>Você está pronto para viajar para lugares incríveis e criar memórias inesquecíveis? Nós temos o prazer de oferecer uma ampla variedade de pacotes de viagem personalizados para atender às suas necessidades e desejos. Seja para um mergulho em águas cristalinas ou para explorar as maravilhas da natureza, nós temos a viagem perfeita para você. Nossa equipe altamente qualificada e experiente está pronta para ajudá-lo a planejar a viagem dos seus sonhos!</p>
+      <Link to='viagens'>
+        <button>Ver Pacotes</button>
+      </Link>
+      </div>
+    </main>
+  )
+}
+
+export default index
---
+++ src/components/ListaViagens/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const index = () => {
+ return (
+  <main>
+      
+   <h1>Lista Viagens</h1>
+   <Link to='/'>
+      Home
+   </Link>
+  </main>
+ );
+};
+
+export default index;
