00da8057f591f6bc94504168da039670e3412abf
MaykGomes92 <mayk_gomes98@hotmail.com>
2023-02-08 18:04:58 -0300

    .

--- src/components/HotelEscolhido/index.tsx
+++ src/components/HotelEscolhido/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./styles.scss";
 import { useParams } from "react-router-dom";
-import { ListaDeHoteis, TypesHoteis } from "../../listaHoteis";
+import { ListaDeHoteis, PropsHoteis, ImagesHoteis } from "../../listaHoteis";
 import Header from "../Header";
 
 const HotelEscolhido = () => {
  const { id } = useParams();
- const [filtrarListaDeHoteis, setFiltrarHoteis] =
-  React.useState<TypesHoteis[]>();
+ const [filtrarListaDeHoteis, setFiltrarHoteis] = React.useState<PropsHoteis[]>(
+  []
+ );
 
  React.useEffect(() => {
   setFiltrarHoteis(
@@ -18,19 +19,31 @@ const HotelEscolhido = () => {
  }, [id]);
 
  return (
-  <>
+  <main className="hotelEscolhidoContent">
    <header className="headerPageHotelEscolhido">
     <Header />
     <h1>Sky Team</h1>
    </header>
    {filtrarListaDeHoteis &&
-    filtrarListaDeHoteis.map((itens: any) => (
-     <div key={itens.title} className="container">
-      <h1>{itens.title}</h1>
-      <p>{itens.descricao}</p>
-     </div>
+    filtrarListaDeHoteis.map((itens) => (
+     <h1 className="localizacaoHotel">{itens.localizacaoHotel}</h1>
     ))}
-  </>
+   <div className="conteudoDaPage">
+    <div className="contentImages">
+     {ImagesHoteis.map((itens, index) => (
+      <div key={index} className="images">
+       <img src={itens} alt="fotos hotel" />
+      </div>
+     ))}
+    </div>
+    <div className="descricaoLeft">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi officia, ipsum numquam provident rem corporis labore nesciunt, non unde nobis, facilis tempore. Amet quos, ad neque qui quae ab!</p>
+    </div>
+    <div className="informacoesRight">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nemo cumque dolorum eveniet provident iste id odit illum molestiae quisquam exercitationem dolores, alias, ex ducimus veritatis ab quae amet eaque.</p>
+    </div>
+   </div>
+  </main>
  );
 };
 
