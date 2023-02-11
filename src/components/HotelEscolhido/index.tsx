175dadd3cf9b767684b48c93b2cd0e4c203283a2
MaykGomes92 <mayk_gomes98@hotmail.com>
2023-02-11 17:58:52 -0300

    .

--- src/components/HotelEscolhido/index.tsx
+++ src/components/HotelEscolhido/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.scss";
 import { useParams } from "react-router-dom";
 import { ListaDeHoteis, PropsHoteis, ImagesHoteis } from "../../listaHoteis";
-import {BiStar} from 'react-icons/bi'
+import { BiStar } from "react-icons/bi";
 import Header from "../Header";
 
 const HotelEscolhido = () => {
@@ -26,8 +26,10 @@ const HotelEscolhido = () => {
     <h1>Sky Teamm2</h1>
    </header>
    {filtrarListaDeHoteis &&
-    filtrarListaDeHoteis.map((itens) => (
-     <h1 className="localizacaoHotel">{itens.localizacaoHotel}</h1>
+    filtrarListaDeHoteis.map((itens, index) => (
+     <h1 key={index} className="localizacaoHotel">
+      {itens.localizacaoHotel}
+     </h1>
     ))}
    <div className="conteudoDaPage">
     <div className="contentImages">
@@ -38,16 +40,79 @@ const HotelEscolhido = () => {
      ))}
     </div>
     <div className="descricaoLeft">
-      <div className="notaHotel">
-        <BiStar/>
-        <BiStar/>
-        <BiStar/>
-        <BiStar/>
-        <BiStar/>
-      </div>
+     <div className="notaHotel">
+      <BiStar />
+      <BiStar />
+      <BiStar />
+      <BiStar />
+      <BiStar />
+     </div>
+     {filtrarListaDeHoteis[0] && (
+      <h1>{filtrarListaDeHoteis[0].localizacaoHotel}</h1>
+     )}
+     <p>Estrada Municipal ********* 801 Bairro ******** - ******, 00000-000</p>
+     <div className="comodidades">
+      <h1>Comodidades</h1>
+      <ul>
+       <li>
+        Número total de quartos:{" "}
+        {filtrarListaDeHoteis[0]
+         ? filtrarListaDeHoteis[0].infoAdicionalQuarto
+         : "555"}
+       </li>
+       <li>Número de andares (edifício principal): 4</li>
+       <li>Suites: 36</li>
+      </ul>
+     </div>
+     <p>mostrar mais</p>
+
+     <div className="informacoesSobre">
+      <h1>
+       Sobre {filtrarListaDeHoteis[0] ? filtrarListaDeHoteis[0].title : "555"}
+      </h1>
+      <p>
+       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt,
+       perspiciatis nisi sed aliquid totam quisquam nostrum qui excepturi
+       quaerat ab recusandae doloribus quo. Optio consectetur pariatur et
+       consequatur eius vel? Lorem, ipsum dolor sit amet consectetur adipisicing
+       elit. Mollitia voluptatem dolore totam expedita a placeat laudantium
+       labore porro et magnam natus tempora ullam voluptatibus, saepe tenetur
+       recusandae quaerat. Suscipit, nulla.
+      </p>
+     </div>
     </div>
     <div className="informacoesRight">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nemo cumque dolorum eveniet provident iste id odit illum molestiae quisquam exercitationem dolores, alias, ex ducimus veritatis ab quae amet eaque.</p>
+     <div className="resumoReserva">
+      <h1>Resumo da Reserva</h1>
+
+      <h2>{filtrarListaDeHoteis[0] ? filtrarListaDeHoteis[0].title : ""}</h2>
+      <h2>Check-in / Check-out</h2>
+      <p>08/05/2023 até 09/05/2023 (1 diária)</p>
+
+      <h2>Hóspedes</h2>
+      <p className="borderBottom">2 Hóspedes (2 adultos)</p>
+
+      <div className="informacaoQuarto">
+       <h2>Quarto 1</h2>
+       <p>Double Standard</p>
+       <span>R$ 382,34</span>
+       <p>Café da manhã</p>
+       <span>Incluso</span>
+       <p>Impostos e Taxas</p>
+       <span>R$ 80,29</span>
+      </div>
+      <div className="contentCompra">
+       <h1>Sua compra</h1>
+       <p>1 diária</p>
+       <span>R$ 462,63</span>
+
+       <h2>Para 1 diária</h2>
+       <span>R$ 462,63</span>
+
+       <button>Reservar agora</button>
+       <button>Pedir desconto</button>
+      </div>
+     </div>
     </div>
    </div>
   </main>
